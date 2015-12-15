a0328bff90417466e40a6a59e3520839d6213a78
Jon Schroeder <jon@redblue.us>
2015-12-15 12:49:43 -0600

    Removing post info from single blog entries

--- wp-content/themes/leadership-isd/single.php
+++ wp-content/themes/leadership-isd/single.php
@@ -6,9 +6,7 @@ add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_content_side
 // Remove the post image (requires HTML5 theme support)
 remove_action( 'genesis_entry_header', 'leadership_isd_featured_photo', 5 );
 
-// add_action( 'genesis_entry_header', 'test_func', 8 );
-// function test_func() {
-// 	echo 'hello!';
-// }
+// Remove post info
+remove_action( 'genesis_entry_header', 'genesis_post_info', 8 );
 
 genesis();
