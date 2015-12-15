c5541e831d7d3d10a5412bf20f5c67821e2ba1d0
Jon Schroeder <jon@redblue.us>
2015-12-15 12:50:07 -0600

    Adding columns to the header so that they can have a button for donations on the right

--- wp-content/themes/leadership-isd/templates/header-logo.php
+++ wp-content/themes/leadership-isd/templates/header-logo.php
@@ -1,3 +1,17 @@
-<section class="logo">
-    <a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="logo"></a>
-</section>
+<div class="header">
+	<div class="one-fourth first">
+		&nbsp;
+	</div>
+	<div class="one-half">
+		<section class="logo">
+		    <a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="logo"></a>
+		</section>
+	</div>
+	<div class="one-fourth">
+		<section class="donate">
+			<a href="/donate" class="button button-small">Donate</a>
+		</section>
+	</div>
+	<div class="clear"></div>
+</div>
+<div class="clear"></div>
