49e3ebf676bb1cbe208400018c455e75bfe7c6b7
Jon Schroeder <jon@redblue.us>
2015-12-15 15:44:02 -0600

    Adding .wrap to the header (it was too wide)

--- wp-content/themes/leadership-isd/templates/header-logo.php
+++ wp-content/themes/leadership-isd/templates/header-logo.php
@@ -1,19 +1,21 @@
 <div class="header">
-	<div class="one-fourth first">
-		<section class="header-left">
-			<?php dynamic_sidebar( 'header-left' ); ?>
-		</section>
+	<div class="wrap">
+		<div class="one-fourth first">
+			<section class="header-left">
+				<?php dynamic_sidebar( 'header-left' ); ?>
+			</section>
+		</div>
+		<div class="one-half">
+			<section class="logo">
+			    <a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="logo"></a>
+			</section>
+		</div>
+		<div class="one-fourth">
+			<section class="donate">
+				<?php dynamic_sidebar( 'header-right' ); ?>
+			</section>
+		</div>
+		<div class="clear"></div>
 	</div>
-	<div class="one-half">
-		<section class="logo">
-		    <a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="logo"></a>
-		</section>
-	</div>
-	<div class="one-fourth">
-		<section class="donate">
-			<?php dynamic_sidebar( 'header-right' ); ?>
-		</section>
-	</div>
-	<div class="clear"></div>
 </div>
 <div class="clear"></div>
