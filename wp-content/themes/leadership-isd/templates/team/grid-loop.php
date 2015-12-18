fb07fc9c4bc4b0d616006df03e16a47fba5e3870
Jon Schroeder <jon@redblue.us>
2015-12-18 17:38:08 -0600

    Get the category (allows this page to be flexible and pull in any category rather than hard-coding it)

--- wp-content/themes/leadership-isd/templates/team/grid-loop.php
+++ wp-content/themes/leadership-isd/templates/team/grid-loop.php
@@ -9,6 +9,13 @@
 </div>
 
 <?php
+
+    // Pull in the category from the selection
+    $category = get_post_meta( get_the_ID(), 'rb_category', true );
+
+    // Take the selection (which is a number), and pull the full category object
+    $category = get_term_by( 'id', $category, 'member-types' );
+
     $our_team_args = array(
         'post_type' => 'members',
         'posts_per_page' => 12,
@@ -17,7 +24,7 @@
             array(
                 'taxonomy' => 'member-types',
                 'field' => 'slug',
-                'terms' => 'our-team'
+                'terms' => $category
             )
         )
     );
@@ -27,20 +34,24 @@
 
 <?php if ( $our_team->have_posts() ) : ?>
     <div class="wrap our-team-grid">
-        <?php while ( $our_team->have_posts() ) : $our_team->the_post(); ?>
+        
+        <?php
+        while ( $our_team->have_posts() ) : $our_team->the_post();
+        
+            $is_first = ($our_team->current_post % 3 === 0) ? "first" : "";
 
-            <?php $is_first = ($our_team->current_post % 3 === 0) ? "first" : ""; ?>
+            include( get_stylesheet_directory() . '/templates/team/grid-item.php' );
 
-            <?php include( get_stylesheet_directory() . '/templates/team/grid-item.php' ); ?>
+        endwhile;
+        ?>
 
-        <?php endwhile; ?>
-
-        <hr>
     </div>
 
     <div class="wrap">
         <?php lisd_posts_navigation($our_team, "Next page", "Previous page"); ?>
     </div>
 
-<?php endif; ?>
-<?php wp_reset_postdata(); ?>
+<?php 
+endif;
+wp_reset_postdata(); 
+?>
