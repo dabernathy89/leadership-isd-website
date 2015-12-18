6e5b0be466af1b007aa3f48d129a377e1c60202a
Jon Schroeder <jon@redblue.us>
2015-12-18 17:38:44 -0600

    Change which image size is pulled in

--- wp-content/themes/leadership-isd/templates/fellows/grid-item.php
+++ wp-content/themes/leadership-isd/templates/fellows/grid-item.php
@@ -1,5 +1,5 @@
 <div class="fellow one-half <?php echo $is_first; ?>">
-    <?php the_post_thumbnail('medium'); ?>
+    <?php the_post_thumbnail('thumbnail'); ?>
     <div class="entry">
         <h3><a href="<?php the_permalink(); ?>">
             <?php the_title(); ?>
