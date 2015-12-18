e3d3b2b6cdff164939a8566d152d834c46047c4f
Jon Schroeder <jon@redblue.us>
2015-12-18 17:36:41 -0600

    Adding permalink to image, customizing the size of that image

--- wp-content/themes/leadership-isd/templates/team/grid-item.php
+++ wp-content/themes/leadership-isd/templates/team/grid-item.php
@@ -1,10 +1,10 @@
 <div class="team-member one-third <?php echo $is_first; ?>">
-    <?php the_post_thumbnail('medium'); ?>
+    <a href="<?php the_permalink();?>"><?php the_post_thumbnail('fellows-square'); ?></a>
     <div class="entry">
         <h3><?php the_title(); ?></h3>
         <p class="title">
             <?php echo get_post_meta( get_the_ID(), '_rbm_members_title', true ); ?>
         </p>
-        <a class="button" href="<?php the_permalink(); ?>">Read bio</a>
+        <a class="button button-small" href="<?php the_permalink(); ?>">Read bio</a>
     </div>
 </div>
