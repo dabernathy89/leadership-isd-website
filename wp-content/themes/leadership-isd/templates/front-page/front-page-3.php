3860d0c2428c4b3c5cb688adf236e453a231e3dd
Jon Schroeder <jon@redblue.us>
2015-12-18 17:36:09 -0600

    Adding 3-column layout to the home page for charts

--- wp-content/themes/leadership-isd/templates/front-page/front-page-3.php
+++ wp-content/themes/leadership-isd/templates/front-page/front-page-3.php
@@ -2,6 +2,9 @@
     <div class="wrap">
         <div class="inner">
             <?php the_field('home_section_3_content'); ?>
+            <div class="one-third first"><?php the_field('home_section_3_content_one'); ?></div>
+            <div class="one-third"><?php the_field('home_section_3_content_two'); ?></div>
+            <div class="one-third"><?php the_field('home_section_3_content_three'); ?></div>
         </div>
     </div>
 </section>
