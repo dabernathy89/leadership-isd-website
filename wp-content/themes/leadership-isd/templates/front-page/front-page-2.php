b1b731cf7d6c6c7f05f855c1c39f7c0a2a856362
Jon Schroeder <jon@redblue.us>
2015-12-19 00:33:43 -0600

    Force crop on images

--- wp-content/themes/leadership-isd/templates/front-page/front-page-2.php
+++ wp-content/themes/leadership-isd/templates/front-page/front-page-2.php
@@ -12,7 +12,7 @@
                 <article>
                     <?php if (has_post_thumbnail()) : ?>
                         <a href="<?php the_permalink(); ?>">
-                            <?php the_post_thumbnail('large'); ?>
+                            <?php the_post_thumbnail( 'fellows-square' ); ?>
                         </a>
                     <?php endif; ?>
                     <h4 class="name">
